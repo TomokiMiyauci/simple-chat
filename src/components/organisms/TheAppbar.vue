cadf393eaa84dd065eb075c60b9626edcb007af0
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-07-24 23:33:53 +0900

    feat(TheAppbar): add to display logged in avatar

--- src/components/organisms/TheAppbar.vue
+++ src/components/organisms/TheAppbar.vue
@@ -4,8 +4,17 @@
       {{ title }}
     </v-toolbar-title>
     <v-spacer />
-    <v-btn icon @click="click">
-      <v-icon>add</v-icon>
+    <v-btn v-if="isLogin" fab absolute right @click="logout">
+      <v-img
+        src="https://vuetifyjs.com/apple-touch-icon-180x180.png"
+        alt="avatar"
+        max-height="40px"
+        max-width="40px"
+      />
+    </v-btn>
+
+    <v-btn v-if="!isLogin" icon @click="click">
+      <v-icon>exit_to_app</v-icon>
     </v-btn>
   </v-app-bar>
 </template>
@@ -14,13 +23,32 @@ import { mapState } from 'vuex'
 import firebase from '~/plugins/firebase'
 
 export default {
+  data() {
+    return {
+      isLogin: null
+    }
+  },
   computed: {
-    ...mapState('navbar', ['title'])
+    ...mapState('navbar', ['title']),
+    idssLogin: function() {
+      return !!firebase.auth().currentUser
+    }
   },
   methods: {
-    click() {
+    async click() {
       const provider = new firebase.auth.GoogleAuthProvider()
-      firebase.auth().signInWithPopup(provider)
+      await firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(() => (this.isLogin = true))
+    },
+    clic() {
+      console.log(!!firebase.auth().currentUser)
+      firebase.auth().signOut()
+    },
+    logout() {
+      firebase.auth().signOut()
+      this.isLogin = false
     }
   }
 }
