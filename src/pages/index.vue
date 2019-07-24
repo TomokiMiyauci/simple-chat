e2f0c92daa6bb3a12290894e92813316f78bbad8
Tomoki Miyauci <development.operation.6.6.6@gmail.com>
2019-07-24 14:33:52 +0900

    feat(index): apply responsive design to component

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,22 @@
 <template>
   <div>
-    <v-layout column pa-1>
-      <v-flex v-for="(message, index) in messages" :key="index" py-3>
-        <v-avatar>
-          <v-img :src="src" />
-        </v-avatar>
-        <v-chip> {{ message.text }} </v-chip>
-        <span class="pl-1 overline">{{ message.timestamp | getHHMM }}</span>
-      </v-flex>
-    </v-layout>
+    <v-container>
+      <v-layout justify-center>
+        <v-flex xs12 sm10 md8 lg6 xl5>
+          <v-layout column pa-1>
+            <v-flex v-for="(message, index) in messages" :key="index" py-3>
+              <v-avatar>
+                <v-img :src="src" />
+              </v-avatar>
+              <v-chip> {{ message.text }} </v-chip>
+              <span class="pl-1 overline">{{
+                message.timestamp | getHHMM
+              }}</span>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
---
+++ src/pages/test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-container>
+      <v-layout justify-center>
+        <v-flex xs12 sm10 md8 lg6 xl4>
+          <p>sfjiojfo</p>
+          <p>sfjiojfo</p>
+          <p>sfjiojfo</p>
+          <p>sfjiojfo</p>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
