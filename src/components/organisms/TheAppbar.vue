662bf16a4190183603df5b2836b4d45da7cbad93
Tomoki Miyauci <development.operation.6.6.6@gmail.com>
2019-07-25 17:11:02 +0900

    feat(TheAppbar): add to display function of user image

--- src/components/organisms/TheAppbar.vue
+++ src/components/organisms/TheAppbar.vue
@@ -4,49 +4,47 @@
       {{ title }}
     </v-toolbar-title>
     <v-spacer />
-    <v-btn v-if="isLogin" fab absolute right @click="logout">
+    <v-btn v-if="isAuth" fab absolute right @click="logout">
       <v-img :src="src" alt="avatar" max-height="40px" max-width="40px" />
     </v-btn>
 
-    <v-btn v-if="!isLogin" icon @click="click">
+    <v-btn v-if="!isAuth" icon @click="click">
       <v-icon>exit_to_app</v-icon>
     </v-btn>
   </v-app-bar>
 </template>
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 import firebase from '~/plugins/firebase'
 
 export default {
-  data() {
-    return {
-      isLogin: null,
-      src: null
-    }
-  },
   computed: {
     ...mapState('navbar', ['title']),
-    idssLogin: function() {
-      return !!firebase.auth().currentUser
+    ...mapState('user', ['photoURL', 'isAuth']),
+
+    src: function() {
+      return this.photoURL
+        ? require(this.photoURL)
+        : require('~/assets/images/anonymous.jpg')
     }
   },
   methods: {
+    ...mapActions('user', ['LOGIN', 'LOGOUT']),
     async click() {
       const provider = new firebase.auth.GoogleAuthProvider()
       await firebase
         .auth()
         .signInWithPopup(provider)
         .then(() => {
-          this.isLogin = true
-          this.src = firebase.auth().currentUser.photoURL
+          this.LOGIN()
         })
     },
-    clic() {
-      console.log(firebase.auth().currentUser.photoURL)
-    },
-    logout() {
-      firebase.auth().signOut()
-      this.isLogin = false
+    async logout() {
+      await firebase
+        .auth()
+        .signOut()
+        .then(() => this.LOGOUT())
+        .catch((error) => console.log(error))
     }
   }
 }
--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,17 +1,39 @@
 <template>
   <div>
-    <div id="firebaseui-auth-container" />
     <v-btn color="success" @click="click">
       text
     </v-btn>
+    <v-dialog v-model="dialog" width="500">
+      <template v-slot:activator="{ on }">
+        <v-btn color="red lighten-2" dark v-on="on">
+          Click Me
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title class="headline justify-center primary-title">
+          Sign In
+        </v-card-title>
+        <div id="firebaseui-auth-container" />
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 import firebase from '~/plugins/firebase'
 
 export default {
+  data() {
+    return {
+      dialog: false
+    }
+  },
+  mounted() {
+    this.init()
+  },
   methods: {
+    ...mapActions(['init']),
     click() {
       const firebaseui = require('firebaseui')
       const ui =
@@ -19,14 +41,18 @@ export default {
         new firebaseui.auth.AuthUI(firebase.auth())
 
       const config = {
-        signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+        signInOptions: [
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ],
         signInSuccessUrl: '/',
         tosUrl: '/tos',
         privacyPolicyUrl: '/privacy-policy'
       }
 
       ui.start('#firebaseui-auth-container', config)
-    }
+    },
+    a() {}
   }
 }
 </script>
