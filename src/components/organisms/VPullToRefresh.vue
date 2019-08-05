9fbba0957fd86b8a6ce9d9ef851598b1a7e7dc00
Tomoki Miyauci <development.operation.6.6.6@gmail.com>
2019-08-05 14:20:37 +0900

    feat(VPullToRefresh): add prototype component of pull to refresh

---
+++ src/components/organisms/VPullToRefresh.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <div
+      class="text-center"
+      :style="{
+        height: diff + 'px',
+        backgroundColor: 'rgba(189, 182, 189, 0.082)',
+        display: isDisplay,
+        maxHeight: '70px'
+      }"
+    >
+      {{ text }}<br />
+      <v-progress-circular
+        :value="(100 / 70) * diff"
+        :rotate="rotate"
+        :indeterminate="indeterminate"
+      >
+      </v-progress-circular>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    callback: {
+      type: Function,
+      required: true
+    }
+  },
+  data() {
+    return {
+      startY: null,
+      endY: null,
+      indeterminate: false
+    }
+  },
+  computed: {
+    isActive: function() {
+      if (this.diff > 70) {
+        return true
+      }
+      return false
+    },
+    diff: function() {
+      return this.endY - this.startY
+    },
+    isDisplay: function() {
+      return this.diff > 0 ? 'block' : 'none'
+    },
+    text: function() {
+      if (this.indeterminate) {
+        return 'Loading...'
+      } else if (this.diff >= 70) {
+        return '↑ Rerease to load'
+      } else {
+        return '↓ Pull to load'
+      }
+    },
+    rotate: function() {
+      return this.indeterminate ? 0 : -90
+    }
+  },
+  beforeMount() {
+    window.addEventListener(
+      'touchstart',
+      (e) => {
+        this.startY = e.touches[0].pageY
+      },
+      { passive: true }
+    )
+    window.addEventListener('touchmove', (e) => {
+      this.endY = e.touches[0].pageY
+    })
+    window.addEventListener('touchend', (e) => {
+      if (this.isActive) {
+        setTimeout(() => {
+          this.callback()
+          this.indeterminate = false
+          this.startY = null
+          this.endY = null
+        }, 1000)
+        this.indeterminate = true
+      } else {
+        this.startY = null
+        this.endY = null
+      }
+    })
+  }
+}
+</script>
