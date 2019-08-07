8f61f7fb772f22494a18e699d21a4e336ee415c9
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-08-08 00:48:18 +0900

    feat(VListItems): add component what is getting Array items

---
+++ src/components/molecules/VListItems.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-list>
+    <v-list-item
+      v-for="(item, index) in items"
+      :key="index"
+      @click="click(item)"
+    >
+      <v-list-item-title
+        ><v-icon class="pr-2">{{ item.icon }}</v-icon
+        >{{ item.text }}</v-list-item-title
+      >
+    </v-list-item>
+  </v-list>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true
+    },
+    click: {
+      type: Function,
+      required: true
+    }
+  }
+}
+</script>
+
+<style></style>
