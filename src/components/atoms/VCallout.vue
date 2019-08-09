44c8cd0a0e81939b7dd1c15e4406f888b3a50545
Tomoki Miyauci <development.operation.6.6.6@gmail.com>
2019-08-09 17:08:17 +0900

    feat(VCallout): add callout component

---
+++ src/components/atoms/VCallout.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- Check if component’s slot is empty -->
+  <div v-if="$slots.default" :class="isLeft ? 'left' : 'right'">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isLeft: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+div {
+  position: relative; /* Set to fix triangle position */
+  display: inline-block;
+  max-width: 300px;
+  margin: 10px 20px 20px;
+  padding: 8px 15px;
+  background: #f0f0f0;
+  text-align: left;
+  border-radius: 17px;
+  word-wrap: break-word;
+  white-space: pre-line;
+}
+div::after {
+  content: '';
+  border: 14px solid transparent;
+  border-top-color: #f0f0f0;
+  position: absolute;
+  top: 0;
+}
+.left::after {
+  left: -10px;
+}
+.right::after {
+  right: -10px;
+}
+</style>
