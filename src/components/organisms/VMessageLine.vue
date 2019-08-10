22db6bda9a4c05775477ff6a66cd0019f0208526
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-08-11 02:57:44 +0900

    feat(VMessageLine): add component of message block

---
+++ src/components/organisms/VMessageLine.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container grid-list-xl style="max-width: 600px;">
+    <v-layout column :align-end="isOwnPost(message)">
+      <v-flex>
+        <v-messageTimestamp :is-own="isOwnPost(message)" :message="message">
+        </v-messageTimestamp>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import VMessageTimestamp from '~/components/molecules/VMessageTimestamp'
+export default {
+  components: {
+    VMessageTimestamp
+  },
+  props: {
+    message: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  computed: {
+    ...mapState('user', ['id'])
+  },
+  methods: {
+    isOwnPost(val) {
+      if (val.userID && this.id === val.userID) {
+        return true
+      }
+      return false
+    }
+  }
+}
+</script>
+
+<style></style>
