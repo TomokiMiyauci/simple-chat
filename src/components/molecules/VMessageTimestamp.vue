735dd435b737fee0143cd93aeaff2ab210eb35b9
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-08-11 02:56:22 +0900

    feat(VMessageTimestamp): add component of combine message and timestamp

---
+++ src/components/molecules/VMessageTimestamp.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <div v-if="isOwn">
+      <div class="timestamp pr-2 overline">
+        {{ message.timestamp | toHHMM }}
+      </div>
+      <v-callout :is-left="false">{{ message.text }}</v-callout>
+    </div>
+    <template v-else>
+      <div class="avatar">
+        <v-avatar>
+          <v-img
+            :src="
+              message.profilePicUrl
+                ? message.profilePicUrl
+                : require('~/assets/images/anonymous.jpg')
+            "
+            alt="avatar"
+          />
+        </v-avatar>
+      </div>
+      <div class="nameMessage">
+        <div class="overline py-1">
+          {{ message.name || 'Anonimus' }}
+        </div>
+        <v-callout>{{ message.text }}</v-callout>
+        <div class="timestamp overline">{{ message.timestamp | toHHMM }}</div>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+import VCallout from '~/components/atoms/VCallout'
+export default {
+  components: {
+    VCallout
+  },
+  props: {
+    message: {
+      type: Object,
+      required: true
+    },
+    isOwn: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.avatar {
+  display: inline;
+  position: absolute;
+}
+
+.nameMessage {
+  display: inline;
+  position: relative;
+  left: 70px;
+}
+.timestamp {
+  display: inline-block;
+}
+</style>
