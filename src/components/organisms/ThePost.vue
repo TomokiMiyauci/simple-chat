ce017abb1d6c1f2a5aa659ebfb43c11d0abff660
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-08-12 01:24:05 +0900

    feat(ThePost): add component of post components putting together

---
+++ src/components/organisms/ThePost.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="post">
+    <v-container grid-list-xs class="container">
+      <v-layout row wrap>
+        <v-flex xs2>
+          <v-post-image></v-post-image>
+        </v-flex>
+        <v-flex xs2>
+          <v-btn icon>
+            <v-icon>mdi-tag</v-icon>
+          </v-btn>
+        </v-flex>
+        <v-flex xs8>
+          <v-text-send></v-text-send>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import VPostImage from '~/components/molecules/VPostImage'
+import VTextSend from '~/components/molecules/VTextSend'
+export default {
+  components: {
+    VTextSend,
+    VPostImage
+  }
+}
+</script>
+
+<style scoped>
+.post {
+  width: 100%;
+  height: 80px;
+}
+.container {
+  max-width: 500px;
+}
+</style>
