8d05ee6c0f1383f972f1d2abb8af222ee09a625a
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-08-12 00:50:43 +0900

    feat(ThePostFooter): add component of message poster fixed footer

---
+++ src/components/organisms/ThePostFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-footer app>
+    <the-post> </the-post>
+  </v-footer>
+</template>
+
+<script>
+import ThePost from '~/components/organisms/ThePost'
+export default {
+  components: {
+    ThePost
+  }
+}
+</script>
+
+<style></style>
