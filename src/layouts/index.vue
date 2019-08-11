bbd37f3d20b587d895392196687a14470eac45cc
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-08-11 19:21:37 +0900

    feat(layout/index): add layout for sub pages

---
+++ src/layouts/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <the-appbar />
+    <v-content>
+      <nuxt />
+    </v-content>
+    <the-bottom-nav></the-bottom-nav>
+  </v-app>
+</template>
+
+<script>
+import TheBottomNav from '~/components/organisms/TheBottomNav'
+import TheAppbar from '~/components/organisms/TheAppbar'
+export default {
+  components: {
+    TheAppbar,
+    TheBottomNav
+  }
+}
+</script>
+
+<style></style>
