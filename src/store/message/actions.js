e932ab38b4361ea84964c12ebe657220b0aaa55d
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-08-21 01:39:10 +0900

    refactor(message/action): export function source of the query

--- src/store/message/actions.js
+++ src/store/message/actions.js
@@ -2,14 +2,14 @@ import { firestoreAction } from 'vuexfire'
 import { QUERY_SNAPSHOT } from './mutation-types'
 import firebase from '~/plugins/firebase'
 
-function baseQuery() {
+export function baseQuery() {
   return firebase
     .firestore()
     .collection('messages')
     .orderBy('timestamp', 'desc')
 }
 export default {
-  [QUERY_SNAPSHOT]({ commit, dispatch }) {
+  [QUERY_SNAPSHOT]({ dispatch }) {
     dispatch('bindMessagesRef')
   },
   bindMessagesRef: firestoreAction(({ bindFirestoreRef, state }) => {
