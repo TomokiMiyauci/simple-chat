84f1c7c9c294713cb25aab0cfa32902ecc9ad2c6
Tomoki Miyauci <development.operation.6.6.6@gmail.com>
2019-08-20 13:10:40 +0900

    feat(VBackIcon): add atoms component of browser back button

---
+++ src/components/atoms/VBackIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-btn icon :to="backTo">
+    <v-icon>mdi-chevron-left</v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+    backTo: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>
+
+<style></style>
