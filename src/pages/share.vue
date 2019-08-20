9427d7764ff40cfaf054166a916cc401ef7706da
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-08-20 23:17:15 +0900

    refactor(share): install app url QRcode

--- src/pages/share.vue
+++ src/pages/share.vue
@@ -1,5 +1,12 @@
 <template>
-  <div></div>
+  <div>
+    <v-container>
+      <h1 class="text-center">Share with friend!</h1>
+      <v-card max-width="375" class="mx-auto">
+        <v-img :src="require('~/assets/images/qrcode.png')"></v-img>
+      </v-card>
+    </v-container>
+  </div>
 </template>
 
 <script>
