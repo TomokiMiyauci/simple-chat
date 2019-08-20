0ea16082e9e817e87e28583be8bd71d0049f8881
Tomoki Miyauci <development.operation.6.6.6@gmail.com>
2019-08-20 18:03:03 +0900

    refactor(index): rewrite mock data to description of app

--- src/pages/rooms/index.vue
+++ src/pages/rooms/index.vue
@@ -1,30 +1,21 @@
 <template>
-  <v-list three-line>
-    <template v-for="(item, index) in items">
-      <v-subheader
-        v-if="item.header"
-        :key="item.header"
-        v-text="item.header"
-      ></v-subheader>
+  <v-card class="mx-auto" flat>
+    <v-list three-line>
+      <template v-for="(item, index) in items">
+        <v-list-item :key="item.title" :to="`/rooms/${item.uuid}`">
+          <v-list-item-avatar>
+            <v-img :src="item.avatar" />
+          </v-list-item-avatar>
 
-      <v-divider
-        v-else-if="item.divider"
-        :key="index"
-        :inset="item.inset"
-      ></v-divider>
-
-      <v-list-item v-else :key="item.title" @click="a">
-        <v-list-item-avatar>
-          <v-img :src="item.avatar"></v-img>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title v-html="item.title"></v-list-item-title>
-          <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-    </template>
-  </v-list>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item-subtitle> {{ item.subtitle }}</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider :key="index" :inset="item.inset" />
+      </template>
+    </v-list>
+  </v-card>
 </template>
 <script>
 export default {
@@ -32,17 +23,13 @@ export default {
   data: () => ({
     items: [
       {
+        uuid: '0000000001',
         avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-        title: 'Brunch this weekend?',
+        title: 'Simple Chat is testing',
         subtitle:
-          "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
+          'This project is currently under development.If you like, please pull request.'
       }
     ]
-  }),
-  methods: {
-    a() {
-      this.$router.push('/rooms/123')
-    }
-  }
+  })
 }
 </script>
