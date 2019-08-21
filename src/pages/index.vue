cd358a92a118ab4e9c406d703c82d83cc8ea41fb
Tomoki Miyauci <development.operation.6.6.6@gmail.com>
2019-08-21 17:01:20 +0900

    refactor(index): redirect to /rooms under development

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,77 +1,11 @@
 <template>
-  <div>
-    <the-pull-to-refresh :callback="loadMore"></the-pull-to-refresh>
-    <the-messages :messages="messages.slice().reverse()"> </the-messages>
-  </div>
+  <div></div>
 </template>
 
 <script>
-import { mapState, mapActions, mapMutations } from 'vuex'
-import ThePullToRefresh from '~/components/organisms/ThePullToRefresh'
-import TheMessages from '~/components/organisms/TheMessages'
-import firebase from '~/plugins/firebase'
-
 export default {
-  components: {
-    TheMessages,
-    ThePullToRefresh
-  },
-  data() {
-    return {
-      lastVisible: null
-    }
-  },
-  computed: {
-    ...mapState('message', ['messages', 'limit', 'isEnd', 'isLoading'])
-  },
   created() {
-    const firstQuery = this.query().limit(this.limit)
-    this.QUERY_SNAPSHOT()
-    firstQuery.get().then((documentSnapshots) => {
-      // Get the last visible document
-      const lastVisible =
-        documentSnapshots.docs[documentSnapshots.docs.length - 1]
-      this.lastVisible = lastVisible
-    })
-  },
-  methods: {
-    ...mapMutations('message', ['GET', 'end', 'loading']),
-    ...mapActions('message', ['QUERY_SNAPSHOT']),
-    queryHandler(ref) {
-      return new Promise((resolve, reject) => {
-        ref.get().then((documentSnapshots) => {
-          if (documentSnapshots.empty) {
-            this.end(true)
-            resolve()
-          }
-          if (this.isEnd) {
-            resolve()
-            return
-          }
-          this.lastVisible = documentSnapshots.docs[documentSnapshots.size - 1]
-          documentSnapshots.forEach((res) => this.GET(res.data()))
-          resolve()
-        })
-      })
-    },
-    loadMore() {
-      return new Promise((resolve, reject) => {
-        const nextQuery = this.query()
-          .startAfter(this.lastVisible)
-          .limit(this.limit)
-        this.loading(true)
-        this.queryHandler(nextQuery).then(() => {
-          this.loading(false)
-          resolve()
-        })
-      })
-    },
-    query() {
-      return firebase
-        .firestore()
-        .collection('messages')
-        .orderBy('timestamp', 'desc')
-    }
+    this.$router.push('/rooms')
   }
 }
 </script>
