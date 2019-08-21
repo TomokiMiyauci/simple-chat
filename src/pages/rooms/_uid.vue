5cc5fde9f23aa295d1ebdc0a796a1dc8ab9d790a
Tomoki Miyauci <development.operation.6.6.6@gmail.com>
2019-08-21 16:52:49 +0900

    refactor(rooms/_uid): remove local functions and import store/message

--- src/pages/rooms/_uid.vue
+++ src/pages/rooms/_uid.vue
@@ -1,64 +1,25 @@
 <template>
-  <div>
-    <the-pull-to-refresh :callback="loadMore"></the-pull-to-refresh>
-    <the-messages :messages="messages.slice().reverse()"> </the-messages>
-  </div>
+  <the-messages :messages="messages.slice().reverse()"> </the-messages>
 </template>
 
 <script>
-import { mapState, mapActions, mapMutations } from 'vuex'
-import { baseQuery } from '~/store/message/actions'
-import ThePullToRefresh from '~/components/organisms/ThePullToRefresh'
+import { mapState, mapActions } from 'vuex'
+import { INIT } from '~/store/message/mutation-types'
 import TheMessages from '~/components/organisms/TheMessages'
 
 export default {
   components: {
-    TheMessages,
-    ThePullToRefresh
+    TheMessages
   },
-  data() {
-    return {
-      lastVisible: null
-    }
-  },
+
   computed: {
-    ...mapState('message', ['messages', 'limit', 'isEnd', 'isLoading'])
+    ...mapState('message', ['messages'])
   },
   created() {
-    this.QUERY_SNAPSHOT()
-    this.init()
+    this.INIT()
   },
   methods: {
-    ...mapMutations('message', ['GET', 'end', 'loading']),
-    ...mapActions('message', ['QUERY_SNAPSHOT']),
-    async init() {
-      const firstQuery = baseQuery().limit(this.limit)
-      const documentSnapshots = await firstQuery.get()
-      // Get the last visible document
-      const lastVisible =
-        documentSnapshots.docs[documentSnapshots.docs.length - 1]
-      this.lastVisible = lastVisible
-    },
-    async queryHandler(ref) {
-      const documentSnapshots = await ref.get()
-      if (documentSnapshots.empty) {
-        this.end(true)
-      }
-      if (this.isEnd) {
-        return
-      }
-      this.lastVisible = documentSnapshots.docs[documentSnapshots.size - 1]
-      documentSnapshots.forEach((res) => this.GET(res.data()))
-    },
-    async loadMore() {
-      const nextQuery = baseQuery()
-        .startAfter(this.lastVisible)
-        .limit(this.limit)
-
-      this.loading(true)
-      await this.queryHandler(nextQuery)
-      this.loading(false)
-    }
+    ...mapActions('message', [INIT])
   }
 }
 </script>
