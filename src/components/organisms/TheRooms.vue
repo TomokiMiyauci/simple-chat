5fd6a382a4f534e8fd844e1fcb39b00a0902435d
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-08-27 01:42:14 +0900

    feat(TheRooms): add main room component

---
+++ src/components/organisms/TheRooms.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card class="mx-auto" flat>
+    <v-list two-line>
+      <template v-for="(item, index) in rooms">
+        <v-list-item :key="item.uid" :to="`/rooms/${item.uid}`">
+          <v-list-item-avatar>
+            <v-img :src="item.avatar" />
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item-subtitle> {{ item.subtitle }}</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider :key="index" :inset="item.inset" />
+      </template>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    rooms: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style></style>
