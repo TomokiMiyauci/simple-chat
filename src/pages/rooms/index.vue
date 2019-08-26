bf869b0428c6fe5397426d4530a4346cebac0118
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-08-27 01:46:47 +0900

    refactor(rooms/index): import component of TheToolbar

--- src/pages/rooms/index.vue
+++ src/pages/rooms/index.vue
@@ -1,35 +1,34 @@
 <template>
-  <v-card class="mx-auto" flat>
-    <v-list three-line>
-      <template v-for="(item, index) in items">
-        <v-list-item :key="item.title" :to="`/rooms/${item.uuid}`">
-          <v-list-item-avatar>
-            <v-img :src="item.avatar" />
-          </v-list-item-avatar>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-            <v-list-item-subtitle> {{ item.subtitle }}</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-        <v-divider :key="index" :inset="item.inset" />
-      </template>
-    </v-list>
-  </v-card>
+  <div>
+    <the-toolbar></the-toolbar>
+    <the-rooms :rooms="rooms"></the-rooms>
+  </div>
 </template>
+
 <script>
+import { mapState, mapMutations, mapActions } from 'vuex'
+import TheRooms from '~/components/organisms/TheRooms'
+import TheToolbar from '~/components/organisms/TheToolbar'
 export default {
+  components: {
+    TheRooms,
+    TheToolbar
+  },
   layout: 'index',
-  data: () => ({
-    items: [
-      {
-        uuid: '0000000001',
-        avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-        title: 'Simple Chat is testing',
-        subtitle:
-          'This project is currently under development.If you like, please pull request.'
-      }
-    ]
-  })
+  computed: {
+    ...mapState('room', ['rooms'])
+  },
+  mounted() {
+    this.load()
+  },
+  destroyed() {
+    this.resetRoom()
+  },
+  methods: {
+    ...mapMutations('room', ['resetRoom']),
+    ...mapActions('room', ['load'])
+  }
 }
 </script>
+
+<style></style>
