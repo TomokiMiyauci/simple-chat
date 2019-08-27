0f1550b3a03d47698b8772a7da0ab2146073e6d3
Tomoki Miyauci <development.operation.6.6.6@gmail.com>
2019-08-27 17:53:36 +0900

    refactor(TheRooms): changed to reflect the most recent message

--- src/components/organisms/TheRooms.vue
+++ src/components/organisms/TheRooms.vue
@@ -4,13 +4,39 @@
       <template v-for="(item, index) in rooms">
         <v-list-item :key="item.uid" :to="`/rooms/${item.uid}`">
           <v-list-item-avatar>
-            <v-img :src="item.avatar" />
+            <v-img
+              :src="
+                item.roomPicUrl
+                  ? item.roomPicUrl
+                  : require('~/assets/images/new.png')
+              "
+            />
           </v-list-item-avatar>
 
           <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-            <v-list-item-subtitle> {{ item.subtitle }}</v-list-item-subtitle>
+            <v-list-item-title>{{
+              item.name ? item.name : 'New Room'
+            }}</v-list-item-title>
+            <v-list-item-subtitle v-if="item.recent" class="pt-1">
+              <v-avatar size="30">
+                <v-img
+                  :src="
+                    item.recent.profilePicUrl
+                      ? item.recent.profilePicUrl
+                      : require('~/assets/images/anonymous.jpg')
+                  "
+              /></v-avatar>
+              {{ item.recent.text }}</v-list-item-subtitle
+            >
+            <v-list-item-subtitle v-else class="pt-1"
+              >No message</v-list-item-subtitle
+            >
           </v-list-item-content>
+          <v-list-item-action>
+            <v-btn icon @click.prevent="click">
+              <v-icon color="grey lighten-1">mdi-qrcode</v-icon>
+            </v-btn>
+          </v-list-item-action>
         </v-list-item>
         <v-divider :key="index" :inset="item.inset" />
       </template>
@@ -24,6 +50,11 @@ export default {
     rooms: {
       type: Array,
       required: true
+    }
+  },
+  methods: {
+    click() {
+      alert()
     }
   }
 }
