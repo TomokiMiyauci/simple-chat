8220d6fa2aaa277794ef78e38b7b21c9f8c76d7e
Tomoki Miyauci <development.operation.6.6.6@gmail.com>
2019-08-28 15:22:42 +0900

    feat(VDialogBtnIcon): add generic dialog launcher button

---
+++ src/components/molecules/VDialogBtnIcon.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-dialog v-model="dialog" width="dialogWidth">
+    <template v-slot:activator="{ on }">
+      <v-btn-icon v-bind="btnIcon" v-on="on"></v-btn-icon>
+    </template>
+    <slot name="content"></slot>
+  </v-dialog>
+</template>
+
+<script>
+import VBtnIcon from '~/components/atoms/VBtnIcon'
+export default {
+  components: {
+    VBtnIcon
+  },
+  props: {
+    dialogWidth: {
+      type: [String, Number],
+      default: 500
+    },
+    btnIcon: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {
+      dialog: false
+    }
+  },
+  methods: {
+    forceClose() {
+      this.dialog = false
+    }
+  }
+}
+</script>
+
+<style></style>
