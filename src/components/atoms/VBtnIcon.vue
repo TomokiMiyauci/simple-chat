4f97d5a7bfa36e2c611f52d389bdd98d3122ecde
Tomoki Miyauci <development.operation.6.6.6@gmail.com>
2019-08-28 15:21:14 +0900

    feat(VBtnIcon): add a generic icon button

---
+++ src/components/atoms/VBtnIcon.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-btn :color="btnColor" :icon="!btnText && !!icon" @click="click">
+    <v-icon v-if="icon">{{ icon }}</v-icon>
+    {{ btnText }}
+  </v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+    btnColor: {
+      type: String,
+      default: ''
+    },
+    btnText: {
+      type: String,
+      default: ''
+    },
+    icon: {
+      type: String,
+      default: ''
+    }
+  },
+
+  methods: {
+    click() {
+      this.$emit('click')
+    }
+  }
+}
+</script>
+
+<style></style>
