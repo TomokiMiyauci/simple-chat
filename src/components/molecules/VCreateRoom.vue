ed635166b27cebcd77aa7f8b5e95460c5f8389d6
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-09-01 21:13:07 +0900

    refactor(VCreateRoom): add v-tabs

--- src/components/molecules/VCreateRoom.vue
+++ src/components/molecules/VCreateRoom.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card>
-    <v-toolbar dark color="primary">
+    <v-toolbar dark>
       <v-btn icon dark @click="click">
         <v-icon>mdi-close</v-icon>
       </v-btn>
@@ -9,6 +9,13 @@
       <v-toolbar-items>
         <v-btn dark text @click="create">Save</v-btn>
       </v-toolbar-items>
+      <template v-slot:extension>
+        <v-tabs v-model="tabs" icons-and-text grow>
+          <v-tab> Recomend <v-icon>mdi-toolbox</v-icon> </v-tab>
+
+          <v-tab> Customize<v-icon>mdi-tools</v-icon> </v-tab>
+        </v-tabs>
+      </template>
     </v-toolbar>
   </v-card>
 </template>
@@ -17,6 +24,11 @@
 import { mapActions } from 'vuex'
 import { CREATE } from '~/store/room/mutation-types'
 export default {
+  data() {
+    return {
+      tabs: null
+    }
+  },
   methods: {
     ...mapActions('room', [CREATE]),
     click() {
