4f1cde6c1f42a5d56eae01b948b59b507643f4f7
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-09-03 01:52:36 +0900

    refactor(VTag): componentize tag poster

---
+++ src/components/molecules/VPostTag.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-bottom-sheet-btn-icon ref="bottomSheet" :btn-icon="btnIcon">
+      <template #content>
+        <v-tag @onload="onload"></v-tag>
+      </template>
+    </v-bottom-sheet-btn-icon>
+  </div>
+</template>
+<script>
+import VTag from '~/components/molecules/VTag'
+import VBottomSheetBtnIcon from '~/components/molecules/VBottomSheetBtnIcon'
+export default {
+  components: { VBottomSheetBtnIcon, VTag },
+  data: () => ({
+    btnIcon: {
+      icon: 'mdi-tag'
+    }
+  }),
+  methods: {
+    onload(payload) {
+      this.$refs.bottomSheet.forceClose()
+      this.btnIcon.btnColor = payload.color
+      this.$emit('onload', payload)
+    }
+  }
+}
+</script>
--- src/components/molecules/VTag.vue
+++ src/components/molecules/VTag.vue
@@ -1,57 +1,52 @@
 <template>
-  <v-speed-dial
-    v-model="fab"
-    direction="top"
-    transition="slide-y-reverse-transition"
-  >
-    <template v-slot:activator>
-      <v-badge v-model="isShowBadge" :color="badgeColor" overlap>
-        <template v-slot:badge>
-          <v-icon dark>{{ badgeIcon }}</v-icon>
-        </template>
-        <v-btn v-model="fab" icon>
-          <v-icon v-if="fab">mdi-close</v-icon>
-          <v-icon v-else>mdi-tag</v-icon>
-        </v-btn>
-      </v-badge>
-    </template>
-    <v-btn
-      v-for="icon in icons"
-      :key="icon.icon"
-      :color="icon.backColor"
-      fab
-      dark
-      small
-      @click="switchBadge(icon)"
-    >
-      <v-icon>{{ icon.icon }}</v-icon>
-    </v-btn>
-  </v-speed-dial>
+  <v-list>
+    <v-subheader>{{ subheader }}</v-subheader>
+    <v-chip-group column class="pl-2">
+      <v-chip
+        v-for="tag in tags"
+        :key="tag.icon"
+        label
+        :color="tag.color"
+        text-color="white"
+        :value="tag"
+        @click="click(tag)"
+      >
+        <v-icon left>{{ tag.icon }}</v-icon
+        >{{ tag.text }}
+      </v-chip>
+    </v-chip-group>
+  </v-list>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      isShowBadge: false,
-      badgeIcon: null,
-      badgeColor: null,
-      fab: false,
-      icons: [
-        { icon: 'mdi-alert', backColor: 'yellow darken-1' },
-        { icon: 'mdi-thumb-down', backColor: 'red darken-1' },
-        { icon: 'mdi-thumb-up', backColor: 'blue darken-1' }
+      checker: null,
+      subheader: 'Chose Tag',
+      tags: [
+        { icon: 'mdi-alert', text: 'Emergency', color: 'purple' },
+        { icon: 'mdi-hand-okay', text: 'OK', color: 'success' },
+        { icon: 'mdi-thumb-up', text: 'Like', color: 'red darken-1' },
+        { icon: 'mdi-thumb-down', text: 'Bad', color: 'blue darken-1' }
       ]
     }
   },
+
   methods: {
-    switchBadge(val) {
-      if (this.badgeIcon === val.icon) {
-        this.isShowBadge = !this.isShowBadge
+    click(payload) {
+      if (this.isDuplicate(payload)) {
+        this.$emit('onload', {})
+        this.checker = null
       } else {
-        this.badgeColor = val.backColor
-        this.badgeIcon = val.icon
-        this.isShowBadge = true
+        this.$emit('onload', payload)
+        this.checker = payload
+      }
+    },
+
+    isDuplicate(payload) {
+      if (this.checker && this.checker.icon === payload.icon) {
+        return true
       }
     }
   }
