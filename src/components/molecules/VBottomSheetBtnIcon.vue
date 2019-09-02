04c75f95391580681d1eb639b7c2a918e5498d01
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-09-03 01:53:22 +0900

    feat(VBottomSheetBtnIcon): add bottom sheet wrapper component

---
+++ src/components/molecules/VBottomSheetBtnIcon.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-bottom-sheet v-model="sheet">
+    <template v-slot:activator="{ on }">
+      <v-btn-icon v-bind="btnIcon" v-on="on"></v-btn-icon>
+    </template>
+    <slot name="content"></slot>
+  </v-bottom-sheet>
+</template>
+
+<script>
+import VBtnIcon from '~/components/atoms/VBtnIcon'
+export default {
+  components: {
+    VBtnIcon
+  },
+  props: {
+    btnIcon: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {
+      sheet: false
+    }
+  },
+  methods: {
+    forceClose() {
+      this.sheet = false
+    }
+  }
+}
+</script>
+
+<style></style>
