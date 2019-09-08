c09201d6e52952bb32e9e13ab73c6b2c9f15de68
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-09-08 21:57:24 +0900

    refactor(VPostTag): remove root div element and add x-large props

--- src/components/molecules/VPostTag.vue
+++ src/components/molecules/VPostTag.vue
@@ -1,11 +1,9 @@
 <template>
-  <div>
-    <v-bottom-sheet-btn-icon ref="bottomSheet" :btn-icon="btnIcon">
-      <template #content>
-        <v-tag-list @onload="onload"></v-tag-list>
-      </template>
-    </v-bottom-sheet-btn-icon>
-  </div>
+  <v-bottom-sheet-btn-icon ref="bottomSheet" :btn-icon="btnIcon">
+    <template #content>
+      <v-tag-list @onload="onload"></v-tag-list>
+    </template>
+  </v-bottom-sheet-btn-icon>
 </template>
 <script>
 import { mapState } from 'vuex'
@@ -18,9 +16,9 @@ export default {
     ...mapState('post', ['tag']),
     btnIcon() {
       if (this.tag) {
-        return { icon: 'mdi-tag', btnColor: this.tag.color }
+        return { icon: 'mdi-tag', btnColor: this.tag.color, xlarge: true }
       } else {
-        return { icon: 'mdi-tag', btnColor: '' }
+        return { icon: 'mdi-tag', btnColor: '', xlarge: true }
       }
     }
   },
