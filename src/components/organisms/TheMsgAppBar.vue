b66b5aa6ffab9662c15232df627e2fbb1e1b3e3b
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-09-15 04:32:43 +0900

    refactor(TheMsgAppBar): add extend app bar when botton clicked

--- src/components/organisms/TheMsgAppBar.vue
+++ src/components/organisms/TheMsgAppBar.vue
@@ -1,13 +1,17 @@
 <template>
-  <v-app-bar dark app>
+  <v-app-bar dark app :extended="extended">
     <v-back-icon v-if="backTo" :back-to="backTo"></v-back-icon>
-    <v-toolbar-title class="text-capitalize">
-      {{ title }}
-    </v-toolbar-title>
+
     <v-spacer />
+    <v-btn icon @click="extended = !extended">
+      <v-icon>mdi-phone-in-talk</v-icon>
+    </v-btn>
     <the-pull-to-refresh></the-pull-to-refresh>
     <the-settings-room></the-settings-room>
     <the-firebase-ui />
+    <template v-if="extended" v-slot:extension>
+      <v-tab-phone-video @click="extended = !extended"></v-tab-phone-video>
+    </template>
   </v-app-bar>
 </template>
 <script>
@@ -16,17 +20,25 @@ import ThePullToRefresh from '~/components/organisms/ThePullToRefresh'
 import VBackIcon from '~/components/atoms/VBackIcon'
 import TheFirebaseUi from '~/components/organisms/TheFirebaseUi'
 import TheSettingsRoom from '~/components/organisms/TheSettingsRoom'
+import VTabPhoneVideo from '~/components/molecules/VTabPhoneVideo'
 export default {
   components: {
     TheFirebaseUi,
     VBackIcon,
     ThePullToRefresh,
-    TheSettingsRoom
+    TheSettingsRoom,
+    VTabPhoneVideo
   },
   props: {
     backTo: {
       type: String,
       default: null
+    }
+  },
+  data() {
+    return {
+      extended: false,
+      dialog: false
     }
   },
   computed: {
