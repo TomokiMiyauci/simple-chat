f6c6106b7b0776bc25b5a50e884cb856083749c6
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-09-19 18:51:45 +0900

    feat(createroom): add component of card what is used for creating room

--- src/components/molecules/VCreateRoom.vue
+++ src/components/molecules/VCreateRoom.vue
@@ -1,32 +1,59 @@
 <template>
   <v-card>
-    <v-toolbar dark>
-      <v-btn icon dark @click="click">
+    <v-toolbar>
+      <v-btn icon @click="click">
         <v-icon>mdi-close</v-icon>
       </v-btn>
       <v-toolbar-title>New Room</v-toolbar-title>
-      <div class="flex-grow-1"></div>
+      <v-spacer></v-spacer>
       <v-toolbar-items>
-        <v-btn dark text @click="create">Save</v-btn>
+        <v-btn :disabled="disabled" text @click="create">Create</v-btn>
       </v-toolbar-items>
-      <template v-slot:extension>
-        <v-tabs v-model="tabs" icons-and-text grow>
-          <v-tab> Recomend <v-icon>mdi-toolbox</v-icon> </v-tab>
-
-          <v-tab> Customize<v-icon>mdi-tools</v-icon> </v-tab>
-        </v-tabs>
-      </template>
     </v-toolbar>
+
+    <v-tabs v-model="select" icons-and-text grow>
+      <v-tab href="#quick"> Quick <v-icon>mdi-clock-fast</v-icon> </v-tab>
+
+      <v-tab href="#custom"> Customize<v-icon>mdi-tools</v-icon> </v-tab>
+
+      <v-tabs-items v-model="select">
+        <v-tab-item value="quick">
+          <v-container class="fill-height" grid-list-xs>
+            <v-row justify="center" align="center">
+              <v-col cols="12" sm="8" md="4">
+                <v-create-room-quickly @valid="valid"></v-create-room-quickly>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-tab-item>
+        <v-tab-item value="custom">
+          <v-container class="fill-height" grid-list-xs>
+            <v-row justify="center" align="center">
+              <v-col cols="12" sm="8" md="4">
+                <v-create-room-custom></v-create-room-custom> </v-col
+            ></v-row>
+          </v-container>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-tabs>
   </v-card>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
+import VCreateRoomQuickly from '~/components/molecules/VCreateRoomQuickly'
+import VCreateRoomCustom from '~/components/molecules/VCreateRoomCustom'
 import { CREATE } from '~/store/room/mutation-types'
 export default {
+  components: {
+    VCreateRoomCustom,
+    VCreateRoomQuickly
+  },
+
   data() {
     return {
-      tabs: null
+      select: null,
+      disabled: false
     }
   },
   methods: {
@@ -37,9 +64,20 @@ export default {
     create() {
       this.CREATE()
       this.click()
+    },
+
+    valid(payload) {
+      this.disabled = payload
     }
   }
 }
 </script>
 
-<style></style>
+<style scoped>
+.v-tab__items,
+.v-window-item,
+.v-window >>> div.v-window__container {
+  /* ここが重要 */
+  height: 100%;
+}
+</style>
---
+++ src/components/molecules/VCreateRoomQuickly.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card>
+    <v-form v-model="valid" @input="input">
+      <v-subheader>Room Icon</v-subheader>
+      <v-img :src="require('~/assets/images/new-room.png')">
+        <v-btn absolute top left color="success">change</v-btn></v-img
+      >
+      <v-card-text>
+        <v-text-field
+          label="Room Name"
+          value="New Room"
+          :rules="[(v) => !!v || 'Room Name is required']"
+        ></v-text-field>
+      </v-card-text>
+    </v-form>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      valid: false
+    }
+  },
+
+  methods: {
+    input() {
+      if (this.valid) {
+        this.$emit('valid', false)
+      } else {
+        this.$emit('valid', true)
+      }
+    }
+  }
+}
+</script>
+<style></style>
