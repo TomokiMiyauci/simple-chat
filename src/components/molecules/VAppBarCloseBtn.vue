b8d9ad0f337781a1f368fca670738675fc55270e
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-09-23 05:39:23 +0900

    feat(vappbarclosebtn): add component of app bar with close button

---
+++ src/components/molecules/VAppBarCloseBtn.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app-bar>
+    <v-btn-icon v-if="!removeBackBtn" icon="mdi-arrow-left" @click="close" />
+    <slot name="content"></slot>
+  </v-app-bar>
+</template>
+
+<script>
+import VBtnIcon from '~/components/atoms/VBtnIcon'
+export default {
+  components: {
+    VBtnIcon
+  },
+
+  props: {
+    removeBackBtn: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  methods: {
+    close() {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style></style>
