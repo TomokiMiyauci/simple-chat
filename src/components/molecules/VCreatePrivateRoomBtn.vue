654767324762fb53f19340453dd89b0ec1db3ae0
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-09-25 02:02:32 +0900

    feat(vcreateprivaterooombtn): add component of create private room dialog button

---
+++ src/components/molecules/VCreatePrivateRoomBtn.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-btn-icon
+    absolute
+    left
+    fab
+    bottom
+    btn-color="purple"
+    icon="mdi-plus-box-multiple"
+    @click="CREATE_PRIVATE"
+  ></v-btn-icon>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import VBtnIcon from '~/components/atoms/VBtnIcon'
+export default {
+  components: {
+    VBtnIcon
+  },
+
+  methods: {
+    ...mapActions('room', ['CREATE_PRIVATE'])
+  }
+}
+</script>
+
+<style></style>
