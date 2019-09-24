7657051cb8766ec4168714603795cd61213de9ad
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-09-24 23:34:01 +0900

    refactor(thebottomnav): apply to disabled props to button

--- src/components/organisms/TheBottomNav.vue
+++ src/components/organisms/TheBottomNav.vue
@@ -1,20 +1,26 @@
 <template>
-  <v-bottom-navigation v-model="bottomNav" dark app fixed>
-    <v-btn v-for="item in items" :key="item.title" :to="item.to">
-      <span>{{ item.title }}</span>
-      <v-icon>{{ item.icon }}</v-icon>
-    </v-btn>
+  <v-bottom-navigation v-model="btmNav" dark app fixed>
+    <v-btn to="/rooms/public" value="public">
+      <span>Public</span> <v-icon>mdi-home</v-icon></v-btn
+    >
+    <v-btn to="/rooms/private" :disabled="!isAuth" value="private">
+      <span>Private</span> <v-icon>mdi-hexagon-slice-2</v-icon></v-btn
+    >
+    <v-btn to="/share" value="share">
+      <span>Share</span> <v-icon>mdi-share-variant</v-icon></v-btn
+    >
   </v-bottom-navigation>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
   data: () => ({
-    bottomNav: null,
-    items: [
-      { title: 'Room', icon: 'mdi-hexagon-slice-2', to: '/rooms' },
-      { title: 'Share', icon: 'mdi-share-variant', to: '/share' }
-    ]
-  })
+    btmNav: null
+  }),
+
+  computed: {
+    ...mapState('user', ['isAuth'])
+  }
 }
 </script>
