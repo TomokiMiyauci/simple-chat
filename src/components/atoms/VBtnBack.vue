5de9f8212ff53248d0106651ab70268a6d5dc74e
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-09-24 14:19:53 +0900

    feat(vbtnback): add component of generic back button

---
+++ src/components/atoms/VBtnBack.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-btn-icon icon="mdi-arrow-left" @click="$router.back()"></v-btn-icon>
+</template>
+
+<script>
+import VBtnIcon from '~/components/atoms/VBtnIcon'
+export default {
+  components: {
+    VBtnIcon
+  }
+}
+</script>
+
+<style></style>
