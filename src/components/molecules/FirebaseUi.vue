c088ab599b64f1760b57071a2a294c5a1163efd3
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-09-27 22:55:48 +0900

    refactor(firebaseui): limit to currently available login methods

--- src/components/molecules/FirebaseUi.vue
+++ src/components/molecules/FirebaseUi.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-card width="300px" height="400px">
-    <p class="text-center">Sign In</p>
+  <v-card width="300px" height="200px">
+    <p class="text-center display-1 font-weight-medium">WELCOME</p>
     <div id="firebaseui-auth-container"></div>
   </v-card>
 </template>
@@ -18,11 +18,11 @@ export default {
       const uiConfig = {
         signInOptions: [
           firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-          firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-          firebase.auth.GithubAuthProvider.PROVIDER_ID,
-          firebase.auth.EmailAuthProvider.PROVIDER_ID,
-          firebase.auth.PhoneAuthProvider.PROVIDER_ID
+          // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+          // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+          // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID
+          // firebase.auth.PhoneAuthProvider.PROVIDER_ID
         ],
         signInSuccessUrl: '/'
       }
