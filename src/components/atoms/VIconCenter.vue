74ab688c71d850e0a59261d45049140f06a97f0f
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-10-03 23:39:21 +0900

    feat(viconcenter): add component of v-icon centerd wrapper

---
+++ src/components/atoms/VIconCenter.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-icon :size="size" :color="color" class="icon">{{ icon }}</v-icon>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      required: true
+    },
+
+    size: {
+      type: [String, Number],
+      required: true
+    },
+
+    color: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.icon {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+}
+</style>
