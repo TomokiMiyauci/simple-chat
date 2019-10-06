b5780541442061d8e5a727e74a935039d86dd35e
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-10-06 18:28:50 +0900

    feat(store/dialog): add store of dialog what manage dialog components status

---
+++ src/store/dialog/mutations.js
@@ -0,0 +1,16 @@
+import { SHOW, HIDE, HIDE_ALL } from './mutation-types'
+export default {
+  [SHOW](state, payload) {
+    if (!state.dialogs.includes(payload)) {
+      state.dialogs.push(payload)
+    }
+  },
+
+  [HIDE](state, payload) {
+    state.dialogs.pop(payload)
+  },
+
+  [HIDE_ALL](state) {
+    state.dialogs.length = 0
+  }
+}
