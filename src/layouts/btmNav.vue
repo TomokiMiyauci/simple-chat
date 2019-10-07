89a5212a482b9f85fa58328426784564e96ecf3e
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-10-07 14:41:27 +0900

    feat: add page of user settings

---
+++ src/layouts/btmNav.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-content>
+      <nuxt />
+    </v-content>
+    <the-bottom-nav></the-bottom-nav>
+  </v-app>
+</template>
+
+<script>
+import TheBottomNav from '~/components/organisms/TheBottomNav'
+export default {
+  components: {
+    TheBottomNav
+  }
+}
+</script>
+
+<style></style>
---
+++ src/pages/user/settings.vue
@@ -0,0 +1,15 @@
+<template>
+  <the-settings></the-settings>
+</template>
+
+<script>
+import TheSettings from '~/components/organisms/TheSettings'
+export default {
+  layout: 'btmNav',
+  components: {
+    TheSettings
+  }
+}
+</script>
+
+<style></style>
