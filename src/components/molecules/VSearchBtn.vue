5e667a177a0a0ac5cd18af781637bd51c14c7603
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-10-08 21:35:23 +0900

    feat: add component of search for rooms or messages button

---
+++ src/components/molecules/VSearchBtn.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <v-btn-icon
+      v-bind="btnIcon"
+      @click="SHOW($options._componentTag)"
+    ></v-btn-icon>
+    <v-dialog-wrapper fullscreen :name="$options._componentTag">
+      <the-search-room></the-search-room>
+    </v-dialog-wrapper>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import VBtnIcon from '~/components/atoms/VBtnIcon'
+import VDialogWrapper from '~/components/molecules/VDialogWrapper'
+import TheSearchRoom from '~/components/organisms/TheSearchRoom'
+export default {
+  components: {
+    VBtnIcon,
+    VDialogWrapper,
+    TheSearchRoom
+  },
+
+  data() {
+    return {
+      btnIcon: {
+        icon: 'mdi-magnify'
+      }
+    }
+  },
+
+  methods: {
+    ...mapActions('dialog', ['SHOW'])
+  }
+}
+</script>
+
+<style></style>
---
+++ src/components/organisms/TheSearchRoom.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card>
+    <v-app-bar-close-btn>
+      <v-text-field
+        v-model="message"
+        hide-details
+        single-line
+        autofocus
+        dense
+        outlined
+        rounded
+        filled
+        placeholder="this feature is in progress."
+        :append-icon="message ? 'mdi-send' : 'mdi-microphone'"
+      ></v-text-field>
+    </v-app-bar-close-btn>
+  </v-card>
+</template>
+
+<script>
+import VAppBarCloseBtn from '~/components/molecules/VAppBarCloseBtn'
+export default {
+  components: {
+    VAppBarCloseBtn
+  },
+
+  data() {
+    return {
+      message: null
+    }
+  }
+}
+</script>
+
+<style></style>
