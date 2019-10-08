ecdb9d4ac13b35bce833d5708a0af6d3c057d43c
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-10-08 14:43:37 +0900

    fix(thebottomnav): rewrite box height

--- src/components/organisms/TheBottomNav.vue
+++ src/components/organisms/TheBottomNav.vue
@@ -1,12 +1,12 @@
 <template>
   <v-bottom-navigation v-model="btmNav" dark app fixed>
-    <v-btn to="/rooms/public" value="public">
+    <v-btn height="56" to="/rooms/public" value="public">
       <span>Public</span> <v-icon>mdi-home</v-icon></v-btn
     >
-    <v-btn to="/rooms/private" :disabled="!isAuth" value="private">
+    <v-btn height="56" to="/rooms/private" :disabled="!isAuth" value="private">
       <span>Private</span> <v-icon>mdi-hexagon-slice-2</v-icon></v-btn
     >
-    <v-btn to="/share" value="share">
+    <v-btn height="56" to="/share" value="share">
       <span>Share</span> <v-icon>mdi-share-variant</v-icon></v-btn
     >
   </v-bottom-navigation>
