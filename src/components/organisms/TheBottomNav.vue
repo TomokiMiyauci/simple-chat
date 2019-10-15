063ad3d079e5045a3d1c12124279dce1871f8d4d
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-10-15 21:58:28 +0900

    refactor(thebottomnav): import login induction component

--- src/components/organisms/TheBottomNav.vue
+++ src/components/organisms/TheBottomNav.vue
@@ -3,9 +3,16 @@
     <v-btn height="56" to="/rooms/public" value="public">
       <span>Public</span> <v-icon>mdi-home</v-icon></v-btn
     >
-    <v-btn height="56" to="/rooms/private" :disabled="!isAuth" value="private">
-      <span>Private</span> <v-icon>mdi-hexagon-slice-2</v-icon></v-btn
-    >
+
+    <v-menu offset-y open-on-hover top :nudge-right="-85" :disabled="isAuth">
+      <template v-slot:activator="{ on }">
+        <v-btn height="56" value="private" v-on="on" @click="login">
+          <span>Private</span> <v-icon>mdi-hexagon-slice-2</v-icon></v-btn
+        >
+      </template>
+      <v-login-induction />
+    </v-menu>
+
     <v-btn height="56" to="/share" value="share">
       <span>Share</span> <v-icon>mdi-share-variant</v-icon></v-btn
     >
@@ -14,13 +21,26 @@
 
 <script>
 import { mapState } from 'vuex'
+import VLoginInduction from '~/components/molecules/VLoginInduction'
 export default {
+  components: {
+    VLoginInduction
+  },
+
   data: () => ({
     btmNav: null
   }),
 
   computed: {
     ...mapState('user', ['isAuth'])
+  },
+
+  methods: {
+    login() {
+      if (this.isAuth) {
+        this.$router.push('/room/private')
+      }
+    }
   }
 }
 </script>
