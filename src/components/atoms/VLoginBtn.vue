e25a03129b9cffd5c9241591a0dec8823190031b
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-10-15 21:55:36 +0900

    feat: add component of login button and login suggestion card

---
+++ src/components/atoms/VLoginBtn.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-btn-icon
+    icon="mdi-login-variant"
+    @click="$router.push('/user/login')"
+  ></v-btn-icon>
+</template>
+
+<script>
+import VBtnIcon from '~/components/atoms/VBtnIcon'
+export default {
+  components: {
+    VBtnIcon
+  }
+}
+</script>
+
+<style></style>
---
+++ src/components/molecules/VLoginInduction.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card max-width="250px">
+    <v-alert outlined type="warning" prominent border="left">
+      This function can be used by logging in.
+    </v-alert>
+    <div class="text-center">
+      <v-login-btn></v-login-btn>
+    </div>
+  </v-card>
+</template>
+
+<script>
+import VLoginBtn from '~/components/atoms/VLoginBtn'
+export default {
+  components: {
+    VLoginBtn
+  }
+}
+</script>
+
+<style></style>
