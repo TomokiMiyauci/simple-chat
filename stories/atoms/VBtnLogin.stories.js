2575fe1e867cd4b839d08f603cd78dbb31d65f42
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-10-30 00:51:19 +0900

    feat(vbtnlogin.stories): add story of vbtnlogin

---
+++ stories/atoms/VBtnLogin.stories.js
@@ -0,0 +1,11 @@
+// import { action } from '@storybook/addon-actions'
+import VBtnLogin from '~/components/atoms/VLoginBtn'
+
+export default {
+  title: 'atoms|VBtnLogin'
+}
+
+export const defaults = () => ({
+  components: { VBtnLogin },
+  template: '<v-btn-login ></v-btn-login>'
+})
