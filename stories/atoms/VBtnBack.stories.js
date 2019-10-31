fe2adf9b2ea3d9b4bf7733dccf56692ef076f42f
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-11-01 00:21:03 +0900

    docs(vbtnback.stories.js): add component to storybook

---
+++ stories/atoms/VBtnBack.stories.js
@@ -0,0 +1,11 @@
+// import { action } from '@storybook/addon-actions'
+import VBtnBack from '~/components/atoms/VBtnBack'
+
+export default {
+  title: 'atoms|VBtnBack'
+}
+
+export const defaults = () => ({
+  components: { VBtnBack },
+  template: '<v-btn-back ></v-btn-back>'
+})
