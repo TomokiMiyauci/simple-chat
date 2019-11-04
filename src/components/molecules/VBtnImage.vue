77f5f95c87a3931765b1f0b062b0819f3586c44c
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-11-04 15:19:31 +0900

    feat(vbtnimage): add tooltip and hovering menu

--- src/components/molecules/VBtnImage.vue
+++ src/components/molecules/VBtnImage.vue
@@ -1,18 +1,32 @@
 <template>
-  <div>
-    <v-btn icon :disabled="disabled" @click="$refs.file.onClick()">
-      <v-icon>{{ icon }}</v-icon>
-    </v-btn>
-    <v-file ref="file" @onload="onLoad"></v-file>
-  </div>
+  <v-menu :disabled="!disabled" offset-y open-on-hover top :nudge-right="-107">
+    <template v-slot:activator="{ on: menu }">
+      <v-tooltip top :disabled="disabled">
+        <template #activator="{on: tooltip}">
+          <v-btn
+            icon
+            :color="color"
+            v-on="{ ...tooltip, ...menu }"
+            @click="onClick"
+          >
+            <v-icon>{{ icon }}</v-icon>
+          </v-btn>
+          <v-file ref="file" @onload="onLoad"></v-file>
+        </template>
+        <span>Image</span>
+      </v-tooltip>
+    </template>
+    <v-login-induction></v-login-induction>
+  </v-menu>
 </template>
 
 <script>
 import VFile from '~/components/atoms/VFile'
-
+import VLoginInduction from '~/components/molecules/VLoginInduction'
 export default {
   components: {
-    VFile
+    VFile,
+    VLoginInduction
   },
 
   props: {
@@ -27,7 +41,22 @@ export default {
     }
   },
 
+  computed: {
+    color() {
+      if (this.disabled) {
+        return 'grey lighten-1'
+      }
+      return ''
+    }
+  },
+
   methods: {
+    onClick() {
+      if (!this.disabled) {
+        this.$refs.file.onClick()
+      }
+    },
+
     onLoad(file) {
       this.$emit('onload', file)
     }
