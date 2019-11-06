90ed0e0d1055964ed3e0c3c4665993a493734442
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-11-07 00:51:46 +0900

    feat(layouts/msg): import footer component for message send toolbar

--- src/layouts/msg.vue
+++ src/layouts/msg.vue
@@ -1,17 +1,22 @@
 <template>
-  <v-app>
+  <v-app style="background: linear-gradient(to top right, skyblue, orange);">
     <the-msg-app-bar></the-msg-app-bar>
     <v-content>
       <nuxt />
     </v-content>
+    <v-footer app padless color="transparent">
+      <the-post> </the-post>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
 import TheMsgAppBar from '~/components/organisms/TheMsgAppBar'
+import ThePost from '~/components/organisms/ThePost'
 export default {
   components: {
-    TheMsgAppBar
+    TheMsgAppBar,
+    ThePost
   }
 }
 </script>
